b697bc18883471d901af8ca0b208ca73cec21468
vagrant <vagrant@homestead>
2020-12-13 20:35:44 +0000

    clean all jobs of seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(CreateDepartmentSeeder::class);
         $this->call(CreateUserSeeder::class);
         //$this->call(CreateTaskSeeder::class);
-        $this->call(CreateDepartmentTaskSeeder::class);
+        //$this->call(CreateDepartmentTaskSeeder::class);
     }
 }
