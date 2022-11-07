2f331d154e594f9279f71c34222e1cbe528037c5
indi9 <milenkovicnkv@gmail.com>
2022-11-07 01:47:36 +0100

    update

--- resources/views/admins/tasks/show.blade.php
+++ resources/views/admins/tasks/show.blade.php
@@ -2,63 +2,46 @@
 
 @section('content')
 
-    <h1>Show Task</h1>
+<h1>Show Task</h1>
 
-    <div class="container login createUser">
-        <div class="wrap-form">
-            <div class="form-header">
-                <i class="fa fa-plus" aria-hidden="true"></i>Kreiraj monitor
-            </div>
-            <div class="form-group">
-                <input type="email" class="form-control" name="email" placeholder="Email" autocomplete="off">
-                @error('email')
-                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                @enderror
-            </div>
 
-            <div class="form-group">
-                <div class="form-group ">
-                    <input type="password" class="form-control"  name="password" placeholder="Šifra" class="form-control @error('password') is-invalid @enderror" required autocomplete="new-password">
-                    @error('password')
-                    <span class="invalid-feedback" role="alert">
-                                         <strong>{{ $message }}</strong>
-                                    </span>
-                    @enderror
-                </div>
 
-                <div class="form-group ">
-                    <input id="password-confirm" type="password" class="form-control"  name="password_confirmation" placeholder="Potvrda Šifre" name="password_confirmation" required autocomplete="new-password">
-                </div>
-            </div>
 
-            <div class="form-group">
-                <select class="custom-select custom-select-lg mb-3" name="department_id">
-                    <option value="0">Svi</option>
-                </select>
-                @error('department_id')
-                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                @enderror
-            </div>
-
-            <div class="form-group">
-                <select class="custom-select custom-select-lg mb-3" name="role">
-                    <option value="monitor" selected>Monitor</option>
-                </select>
-                @error('role')
-                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                @enderror
-            </div>
-
-            <!-- comments -->
-
+<div class="container login createUser">
+    <div class="wrap-form">
+        <div class="form-header">
+            <i class="fa fa-hashtag" aria-hidden="true"></i>{{$job->number}}
         </div>
-    </div>
+        <form action="{{route('admin.sektors.store')}}" method="post" role="form">
+            @csrf
 
+            <div class="form-group">
+                <input type="text" class="form-control" name="name" placeholder="Ime sektora">
+            </div>
+
+        </form>
+
+    </div>
+</div>
+
+
+</br>
+<!-- Comment -->
+<div class="container login createUser">
+    <div class="wrap-form">
+        <div class="form-header">
+            <i class="fa fa-plus" aria-hidden="true"></i>Kreiraj sektor
+        </div>
+        <form action="{{route('admin.sektors.store')}}" method="post" role="form">
+            @csrf
+
+            <div class="form-group">
+                <input type="text" class="form-control" name="name" placeholder="Ime sektora">
+            </div>
+            <button type="submit" name="submit" class="btn btn-primary">Kreiraj sektor</button>
+        </form>
+
+    </div>
+</div>
 
 @endsection
