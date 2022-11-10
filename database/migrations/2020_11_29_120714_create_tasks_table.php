d14b4d38c3f75f4735b7faedde1b497e5d109d50
indi9 <milenkovicnkv@gmail.com>
2022-11-10 10:38:27 +0100

    Dodata nova kolona u svim migravijama InnoDB

--- database/migrations/2020_11_29_120714_create_tasks_table.php
+++ database/migrations/2020_11_29_120714_create_tasks_table.php
@@ -14,6 +14,7 @@ class CreateTasksTable extends Migration
     public function up()
     {
         Schema::create('tasks', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
             $table->increments('id');
             $table->integer('number')->unique();
             $table->unsignedInteger('user_id');  //manager(users)
--- database/migrations/2020_11_29_131251_create_department_task_table.php
+++ database/migrations/2020_11_29_131251_create_department_task_table.php
@@ -14,6 +14,7 @@ class CreateDepartmentTaskTable extends Migration
     public function up()
     {
         Schema::create('department_task', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
             $table->increments('id');
             $table->integer('task_id')->unsigned();
             $table->integer('department_id')->unsigned();
